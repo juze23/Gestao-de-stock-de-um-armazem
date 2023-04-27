53e16cf45fa0d4e5a4645c20d47182573ad8244d
juze23 <131892783+juze23@users.noreply.github.com>
2023-04-27 18:26:32 +0100

    branch incompleta da segunda linha

--- AdicionarProduto.c
+++ AdicionarProduto.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "structs.h"
 
-void AdicionarProduto(int *num_produtos, PRODUTO *produtos[MAX_produtos]){  //recebe o ponteiro para o numero de produtos, da struct e do tamanho máximo da struct
+int AdicionarProduto(int *num_produtos, PRODUTO *produtos){  //recebe o ponteiro para o numero de produtos, da struct e do tamanho máximo da struct
 
     int codigo;         //variável para receber novo código
     int i=0;            //variável para usar no for
@@ -12,35 +12,35 @@ void AdicionarProduto(int *num_produtos, PRODUTO *produtos[MAX_produtos]){  //re
     scanf("%d", &codigo);                       //guarda o código do novo produto
 
     for(i = 0; i < *num_produtos; i++){      //correr os produtos todos já inseridos com a variável i e o ponteiro para o numero de produtos
-        if(produtos[i]->product_code == codigo){       //se o codigo do produto for igual a um codigo já inserido
+        if(produtos[i].product_code == codigo){       //se o codigo do produto for igual a um codigo já inserido
             
             printf("Ja existe um produto com esse codigo!\n");      //já existe esse produto
             system("pause");                                        //system pause pq se não o programa limpa logo e não se vê a mensagem
             return;                                             //retorna à main
         }
     }
-    //PAULO
-    produtos[*num_produtos]->product_code = codigo;    //se não, o codigo da variavel PRODUTO toma o valor do novo codigo e são pedidos os restantes dados do produto
+
+    produtos[*num_produtos].product_code = codigo;    //se não, o codigo da variavel PRODUTO toma o valor do novo codigo e são pedidos os restantes dados do produto
 
     printf("Insira o nome do produto: ");
     fflush(stdin);
-    scanf("%[^\n]s", produtos[*num_produtos]->product_name);
+    scanf("%[^\n]s", produtos[*num_produtos].product_name);
 
     printf("Insira a categoria do produto: ");
     fflush(stdin);
-    scanf("%[^\n]s", produtos[*num_produtos]->categoria);
+    scanf("%[^\n]s", produtos[*num_produtos].categoria);
 
     //  printf("Insira a data de validade do produto: ")
     //  scanf("%?", novo_produto.data de validae)
     
     printf("Insira a quantidade: ");
-    scanf("%d", &produtos[*num_produtos]->quantidade);
+    scanf("%d", &produtos[*num_produtos].quantidade);
 
     printf("Insira o preco de compra do produto (sem usar virgulas): ");
-    scanf("%f", &produtos[*num_produtos]->compra);
+    scanf("%f", &produtos[*num_produtos].compra);
 
     printf("Insira o preco de venda do produto (sem usar virgulas): ");
-    scanf("%f", &produtos[*num_produtos]->venda);
+    scanf("%f", &produtos[*num_produtos].venda);
 
     //são utilizados apontadores para o numero de produtos para receber os dados pois estes apontam para a variável criada na main
 
---
+++ AtualizarProduto.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "structs.h"
+
+int pesquisaBin(int codigoA, PRODUTO *produtos, int *num_produtos){
+
+     int ini=0, meio=0, fim=*num_produtos-1;           //pesquisa binária para encontrar a posição do código
+
+    while(ini<=fim){
+
+        meio=((ini+fim)/2);
+
+        if(codigoA==produtos[meio].product_code){
+
+            return meio;
+        }
+        if(codigoA<produtos[meio].product_code){
+
+            fim=meio-1;
+        }
+        else{
+
+            ini=meio+1;
+        }
+    }
+    return -1;
+}
+
+
+int AtualizarProduto(int *num_produtos, PRODUTO *produtos){
+
+    int codigoA=0;
+    int pos=0;
+    int op=0;
+    int i=0;
+
+    printf("Insira o codigo do produto que deseja atualizar:\n");
+    scanf("%d", &codigoA);                  //saber que produto deseja alterar através do código
+
+   pos=pesquisaBin(codigoA, produtos, num_produtos);
+
+   if(pos==-1){
+
+    printf("Produto nao encontrado");
+    return;
+   }
+
+    printf("O que deseja atualizar do produto?");
+    printf("\n1 - Quantidade do produto");
+    printf("\n2 - Preco de venda");
+    printf("\n3 - Ambos");
+    printf("\n> ");
+    scanf("%d", &op);
+
+    switch (op){
+
+        case 1:
+            printf("Insira a nova quantidade do produto:\n");
+            scanf("%d", &produtos[pos].quantidade);
+            break;
+        case 2:
+            printf("Insira o novo preco de venda do produto:\n");
+            scanf("%f", &produtos[pos].venda);
+            break;
+        case 3:
+            printf("Insira a nova quantidade do produto:\n");
+            scanf("%d", &produtos[pos].quantidade);
+            printf("Insira o novo preco de venda do produto:\n");
+            scanf("%f", &produtos[pos].venda);
+            break;
+        default:
+            printf("Nao existe essa opcao");
+            break;
+        }
+    printf("Atualizacao feita");
+}
